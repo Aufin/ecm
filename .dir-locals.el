7e97cc4b3f51e4e9e819a9b31e65f419b6dfa6bd
Drew Crampsie <drewc@Drews-MacBook-Pro.local>
2024-06-03 16:01:55 -0700

    Start change of SPA location and more

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,4 +6,40 @@
 	 (eval . (setq-local
                   org-roam-db-location (expand-file-name
 					"org-roam.db"
-					org-roam-directory))))))
+					org-roam-directory)))
+
+	 ;; https://www.orgroam.com/manual.html#org_002droam_002ddailies
+
+	 ;; For org-roam-dailies to work, you need to define two variables:
+	 
+	 ;; Variable: org-roam-dailies-directory
+	 
+	 ;;     Path to daily-notes. This path is relative to org-roam-directory. 
+	 
+	 ;; Variable: org-roam-dailies-capture-templates
+	 
+	 ;;     Capture templates for daily-notes in Org-roam. 
+
+	 ;; Here is a sane default configuration:
+
+	 ;; (setq org-roam-dailies-directory "daily/")
+
+	 ;; (setq org-roam-dailies-capture-templates
+	 ;;       '(("d" "default" entry
+	 ;;          "* %?"
+	 ;;          :target (file+head "%<%Y-%m-%d>.org"
+	 ;;                             "#+title: %<%Y-%m-%d>\n"))))
+	 (eval . (setq-local
+                  org-roam-dailies-capture-templates
+		        '(("d" "default" entry
+	          "* %?"
+	          :target (file+head "%<%Y-%m-%d>.org"
+	                             "#+title: %<%Y-%m-%d>\n")))))
+	  (eval . (setq-local
+                  org-roam-dailies-directory (expand-file-name
+					      "daily/"
+					      org-roam-directory)))
+
+
+
+	 )))
